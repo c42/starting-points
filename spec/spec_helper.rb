6c819bff7cbc1e471975406050eff4b44a3678aa
Sidu Ponnappa <ckponnappa@gmail.com>
2014-07-09 12:49:56 +0530

    remove the un necessary require

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
-Dir[File.join("#{File.expand_path(File.dirname(__FILE__))}", "..", "..", "lib", "*.rb")].map {|f| require f}
 Dir[File.join("#{File.expand_path(File.dirname(__FILE__))}","..", "lib", "*.rb")].map {|f| require f}
 RSpec.configure do |config|
   
